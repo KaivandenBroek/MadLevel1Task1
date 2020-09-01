777c131c202ee253a3483163ca61a052f8163a64
Kai <Kaivandenbroek@hotmail.com>
2020-09-01 14:08:49 +0200

    layout

--- app/src/main/java/com/example/madlevel1task1/HigherLowerActivity.kt
+++ app/src/main/java/com/example/madlevel1task1/HigherLowerActivity.kt
@@ -2,10 +2,85 @@ package com.example.madlevel1task1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.madlevel1task1.databinding.ActivityHigherLowerBinding
 
 class HigherLowerActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHigherLowerBinding
+    private var currentThrow: Int = 1
+    private var lastThrow: Int = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_higher_lower)
+        binding = ActivityHigherLowerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initViews()
+    }
+
+    /**
+     * Set the initial (UI) state of the game.
+     */
+    private fun initViews() {
+        binding.btnLower.setOnClickListener { onLowerClick() }
+        binding.btnEquals.setOnClickListener { onEqualClick() }
+        binding.btnHigher.setOnClickListener { onHigherClick() }
+        updateUI()
+    }
+
+    private fun updateUI() {
+        binding.lastThrow.text = getString(R.string.last_throw, lastThrow)
+
+        when (currentThrow) {
+            1 -> binding.imageView.setImageResource(R.drawable.dice1)
+            2 -> binding.imageView.setImageResource(R.drawable.dice2)
+            3 -> binding.imageView.setImageResource(R.drawable.dice3)
+            4 -> binding.imageView.setImageResource(R.drawable.dice4)
+            5 -> binding.imageView.setImageResource(R.drawable.dice5)
+            6 -> binding.imageView.setImageResource(R.drawable.dice6)
+        }
+    }
+
+    private fun rollDice() {
+        lastThrow = currentThrow
+        currentThrow = (1..6).random()
+        updateUI()
+    }
+
+    /**
+     * Calls [rollDice] and checks if the answer is correct.
+     */
+    private fun onHigherClick() {
+        rollDice()
+    }
+
+    /**
+     * Calls [rollDice] and checks if the answer is correct.
+     */
+    private fun onLowerClick() {
+        rollDice()
+    }
+
+    /**
+     * Calls [rollDice] and checks if the answer is correct.
+     */
+    private fun onEqualClick() {
+        rollDice()
+    }
+
+    /**
+     * Displays a successful Toast message.
+     */
+    private fun onAnswerCorrect() {
+        val toast = Toast.makeText(applicationContext, R.string.correct, Toast.LENGTH_SHORT)
+        toast.show()
+
+    }
+
+    /**
+     * Displays an incorrect Toast message.
+     */
+    private fun onAnswerIncorrect() {
+        val toast = Toast.makeText(applicationContext, R.string.incorrect, Toast.LENGTH_SHORT)
     }
 }
